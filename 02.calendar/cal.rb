8ce3890b820d84d1956804160c832353934305cc
yota sato <jingangsihaiuei@gmail.com>
2023-10-12 13:50:25 +0900

    cal.rbの修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -9,8 +9,14 @@ def get_date #日付を受け取るメソッド
     opt.on("-m [VAL]"){|v| date[:m] = v}
     opt.parse(ARGV)
     #dateが入力されているかの確認
-    if date[:y].nil? || date[:m].nil?
+    if date[:y].nil? && date[:m].nil?
         y = Date.today.year
+        m = Date.today.month
+    elsif date[:y].nil?
+        y = Date.today.year
+        m = date[:m].to_i
+    elsif date[:m].nil?  
+        y = date[:y].to_i
         m = Date.today.month
     else
         y = date[:y].to_i
@@ -29,26 +35,11 @@ end
 def mk_cal(day_last,day_first) #カレンダー作成メソッド
     puts "      #{day_first.mon}月#{day_first.year}"
     puts " 日 月 火 水 木 金 土"
-    case day_first.wday
-    when 1
-        mk_blank(1)
-    when 2
-        mk_blank(2)
-    when 3
-        mk_blank(3)
-    when 4
-        mk_blank(4)
-    when 5
-        mk_blank(5)
-    when 6  
-        mk_blank(6)
-    end 
-
+    mk_blank(day_first.wday)
+    
     (day_first..day_last).each do |day|
       print sprintf(" %2d",day.day)
-        if day.saturday?
-            puts ""
-        end
+      puts "" if day.saturday?
     end
 end
 day_last,day_first = get_date
