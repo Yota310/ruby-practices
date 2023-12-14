33d392baa278d80a59d83b814fb851632496a13c
yota sato <jingangsihaiuei@gmail.com>
2023-12-14 13:35:09 +0900

    ls.rbのインデントをrubocopをもとに修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 def get_file(files)
   Dir.glob('./*').each do |path|
     files.push(File.basename(path)) if File.file?(path) # ファイル名を出力
   end
 end
 
-def set_up_file(files)
+def setup_file(files)
   max_col = 3
   row = files.size / max_col + 1
-  outputs = Array.new(row).map{Array.new(max_col)}
+  outputs = Array.new(row).map { Array.new(max_col) }
   i = 0
-  files.each_with_index do |file,index|
-    num = index % row
-   i += 1 if num == 0 && index != 0
-   outputs[num][i] = file
+  files.each_with_index do |file, index|
+    num = index % row # 配列の添字に対応させるための計算
+    i += 1 if num.zero? && index != 0
+    outputs[num][i] = file
   end
   outputs
 end
@@ -20,7 +22,7 @@ end
 def outputs_file(outputs)
   outputs.each do |output|
     output.each do |name|
-      print sprintf("%-10s", name)
+      print format('%-10s', name)
     end
     puts
   end
@@ -29,5 +31,5 @@ end
 files = []
 
 get_file(files)
-outputs = set_up_file(files)
+outputs = setup_file(files)
 outputs_file(outputs)
