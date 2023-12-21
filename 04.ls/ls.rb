6dde9fe0caff218ef6ef7bb1d839a7a52c183d32
yota sato <jingangsihaiuei@gmail.com>
2023-12-21 14:17:44 +0900

    ファイルの文字数による表示のずれの修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,29 +1,31 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def get_file(files)
+def get_files(files)
   Dir.glob('./*').each do |path|
     files.push(File.basename(path)) if File.file?(path) # ファイル名を出力
   end
 end
 
-def setup_file(files)
+def setup_files(files)
   max_col = 3
+  maxsize = 0
   row = files.size / max_col + 1
-  outputs = Array.new(row).map { Array.new(max_col) }
+  output = Array.new(row).map { Array.new(max_col) }
   i = 0
   files.each_with_index do |file, index|
     num = index % row # 配列の添字に対応させるための計算
     i += 1 if num.zero? && index != 0
-    outputs[num][i] = file
+    output[num][i] = file
+    maxsize = file.size if maxsize < file.size
   end
-  outputs
+  [output,maxsize]
 end
 
-def outputs_file(outputs)
-  outputs.each do |output|
+def output_files(output,maxsize)
+  output.each do |output|
     output.each do |name|
-      print format('%-10s', name)
+      print format("%-#{maxsize + 1}s", name)
     end
     puts
   end
@@ -31,6 +33,6 @@ end
 
 files = []
 
-get_file(files)
-outputs = setup_file(files)
-outputs_file(outputs)
+get_files(files)
+output,maxsize = setup_files(files)
+output_files(output,maxsize)
