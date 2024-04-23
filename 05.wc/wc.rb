aedda4ca8f16e87afc32ea3b63dee5cb62e33ff8
yota sato <jingangsihaiuei@gmail.com>
2024-04-23 15:53:23 +0900

    メソッドの分解

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -5,57 +5,57 @@ require 'optparse'
 def judge_option_state
   opt = OptionParser.new
   option_state = {}
-  parse_num = ARGV.find_index {|argv| /\./.match?(argv) }
   opt.on('-l') { |v| option_state[:l] = v }
   opt.on('-w') { |v| option_state[:w] = v }
   opt.on('-c') { |v| option_state[:c] = v }
-  opt.parse(ARGV[..parse_num]) 
+  opt.parse!(ARGV)
   option_state
 end
 
-def setup_import_files
-  files = []
-  size = []
-  argv_array = Dir.glob(ARGV)
-  argv_array.delete_at(0) if argv_array[0].match('-')
-  if !argv_array.empty?
-    argv_array.each do |argv|
-      File.open(argv, 'r') { |f| 
-      files.push(f.read)
-      size.push(f.size)
-      }    
+def set_up_import_files
+  array_files = []
+  filename_array = Dir.glob(ARGV)
+  if !filename_array.empty?
+    filename_array.each do |argv|
+      File.open(argv, 'r') do |f|
+        name = argv
+        argv = {}
+        argv[:name] = name
+        argv[:content] = f.read
+        argv[:size] = f.size
+        array_files.push(argv)
+      end
     end
   else
     file = $stdin.read
-    files.push(file)
-    size.push(file.bytesize)
+    hash_file[:content] = file
+    hash_file[:size] = file.bytesize
+    hash_files.push(hash_file)
   end
-  [size, files, argv_array]
+  [array_files, filename_array]
 end
 
-def setup_result(size, file, option_state, i, argv_array)
+def set_up_result(size, file, filename)
   result = {}
   result[:l] = file.count("\n")
   result[:w] = file.split(' ').count
   result[:c] = size
-  result[:name] = argv_array[i]
-  result.delete(:l) if option_state[:l].nil? && !option_state.empty?
-  result.delete(:w) if option_state[:w].nil? && !option_state.empty?
-  result.delete(:c) if option_state[:c].nil? && !option_state.empty?
+  result[:name] = filename
   result
 end
 
-def setup_total_num(results, option_state, argv_array)
-  total = {}
-
-  total[:l] = results.sum { |result| result[:l] } if option_state[:l] || option_state.empty?
-  total[:w] = results.sum { |result| result[:w] } if option_state[:w] || option_state.empty?
-  total[:c] = results.sum { |result| result[:c] } if option_state[:c] || option_state.empty?
-  total[:name] = 'total' if total[:l] || total[:w] || total[:c] || argv_array.size > 1
+def set_up_total_num(results, _option_state, _filename_array, total)
+  total[:l] = results.sum { |result| result[:l] }
+  total[:w] = results.sum { |result| result[:w] }
+  total[:c] = results.sum { |result| result[:c] }
+  total[:name] = 'total'
   total
 end
 
-def output_results(result)
+def output_results(result, option_state)
+  result.delete(:l) if option_state[:l].nil? && !option_state.empty?
+  result.delete(:w) if option_state[:w].nil? && !option_state.empty?
+  result.delete(:c) if option_state[:c].nil? && !option_state.empty?
   result.each_value do |output|
     print format('%8s', output)
   end
@@ -66,14 +66,14 @@ def main
   results = []
   total = {}
   option_state = judge_option_state
-  size, files, argv_array = setup_import_files
-  files.each_with_index do |file, i|
-    results.push(setup_result(size[i], file, option_state,i, argv_array))
+  array_files, argv_array = set_up_import_files
+  array_files.each do |file|
+    results.push(set_up_result(file[:size], file[:content], file[:name]))
   end
-  total = setup_total_num(results, option_state, argv_array)
+  total = set_up_total_num(results, option_state, argv_array, total)
   results.push(total) if results.size > 1
   results.each do |result|
-    output_results(result)
+    output_results(result, option_state)
   end
 end
 
